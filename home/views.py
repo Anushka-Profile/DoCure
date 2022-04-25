e4a347e334aa1b2a4d39c141b62c50f1533c51f1
Tirth11 <59504521+Tirth11@users.noreply.github.com>
2022-04-25 20:03:16 +0530

    deploy42

--- home/views.py
+++ home/views.py
@@ -893,9 +893,28 @@ def password_reset_request(request):
                                     if password_reset_form.is_valid():
                                         data = password_reset_form.cleaned_data['email']
                                         associated_users = User.objects.filter(Q(email=data))
-                                        associated_userss = ConfirmDoctor.objects.filter(Q(email=data))
+                                        associated_userss = Doctor.objects.filter(Q(email=data))
                                         if associated_users.exists():
                                             for user in associated_users:
+                                                subject = "Password Reset Requested"
+                                                email_template_name = "HtmlFiles/password_reset_email.txt"
+                                                c = {
+                                                "email":user.email,
+                                                'domain':'127.0.0.1:8000',
+                                                'site_name': 'Website',
+                                                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                                                "user": user,
+                                                'token': default_token_generator.make_token(user),
+                                                'protocol': 'http',
+                                                }
+                                                email = render_to_string(email_template_name, c)
+                                                try:
+                                                    send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+                                                except BadHeaderError:
+                                                    return HttpResponse('Invalid header found.')
+                                                return redirect ("/password_reset/done/")
+                                        if associated_userss.exists():
+                                            for user in associated_userss:
                                                 subject = "Password Reset Requested"
                                                 email_template_name = "HtmlFiles/password_reset_email.txt"
                                                 c = {
